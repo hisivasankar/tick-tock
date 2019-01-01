5469070fe9d1e2c410644cebe32ef07f14980337
Sivasankar <sivasankar.d@sap.com>
2019-01-01 14:50:14 +0530

    Updates

---
+++ precache-manifest.80e403a8a12979c9fb780642d7ede823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2062cc0d239ea5c1132",
+    "url": "/tick-tock/static/js/runtime~main.d2062cc0.js"
+  },
+  {
+    "revision": "7640e009dfdfbe949ec9",
+    "url": "/tick-tock/static/js/main.7640e009.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/tick-tock/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "7640e009dfdfbe949ec9",
+    "url": "/tick-tock/static/css/main.569d52e2.chunk.css"
+  },
+  {
+    "revision": "3b14780b10d368aed6b53b0980d951dd",
+    "url": "/tick-tock/index.html"
+  }
+];
